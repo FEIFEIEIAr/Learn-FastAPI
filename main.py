d4237d8f1c1504e0830c8740d2c574e1cb37a968
FEIFEIEIAr <912704652@qq.com>
2023-01-17 16:11:47 +0800

    Changes to be committed: 	modified:   .gitignore 	new file:   app/api/Base.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Body, Form
+from fastapi.responses import HTMLResponse, JSONResponse, FileResponse, RedirectResponse, Response
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    html_content = """
+    <html>
+        <body><p style="color:red">Hello World</p></body>
+    </html>"""
+    return HTMLResponse(content=html_content, status_code=200)
+
+# @app.get("/user/{user_id}")   # http://127.0.0.1:8000/user/7
+@app.get("/user")  # http://127.0.0.1:8000/user?user_id=7
+def users(user_id, token=Header(None)):
+    return {"message": "Hello Users", "user_id": user_id, "token": token}
+
+@app.route("/login", methods=["POST", "GET", "PUT"])
+def login(username=Form(None), password=Form(None)):
+    return {"data": {"username": username, "password": password}}
 
 
 if __name__=='__main__':
